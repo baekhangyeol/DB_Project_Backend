caa899c698865de351cefa7a71a6146c77bcf4fa
hangyeol <snake011128@naver.com>
2023-11-04 03:48:34 +0900

    Feat: Car, CarOption, CarType 모델 생성

--- car/models.py
+++ car/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class CarType(models.Model):
+    brand = models.CharField(max_length=100, verbose_name='차량 브랜드')
+    SIZE_CHOICES = {
+        ('small', '소형'),
+        ('medium', '중형'),
+        ('large', '대형'),
+    }
+    size = models.CharField(max_length=6, choices=SIZE_CHOICES, verbose_name='차량 크기')
+
+    class Meta:
+        verbose_name = '차량 유형'
+
+
+class CarOption(models.Model):
+    option_name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = '차량 옵션'
+
+
+class Car(models.Model):
+    car_type = models.ForeignKey(CarType, on_delete=models.CASCADE)
+    mileage = models.FloatField(default=0.0)
+    availability = models.BooleanField(default=True)
+    rental_price = models.IntegerField(default=0)
+    options = models.ForeignKey(CarOption, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = '차량'
