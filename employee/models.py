3d24c677ac4b4a4fc5560166b8ece6a39ae1d3ae
hangyeol <snake011128@naver.com>
2023-11-04 15:41:52 +0900

    Feat: 직원, 지점 모델 생성

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Branch(models.Model):
+    phone_number = models.CharField(max_length=15)
+    address = models.TextField()
+
+    class Meta:
+        verbose_name = '지점'
+        verbose_name_plural = '지점들'
+
+
+class Employee(models.Model):
+    branch = models.ForeignKey(Branch, related_name='employees', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    email = models.EmailField()
+    bank_account = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=0, default=30000000)
